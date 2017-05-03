5015bbfc4095f68367980cf3c633a052d2572ef8
lkcz04185 <lkcz04185@gmail.com>
2017-05-03 12:54:14 +0000

    stats_page

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -1,27 +1,22 @@
 class StatsController < ApplicationController
 
   def index
+
     if params[:from] && params[:to]
       from = Date.parse(params[:from])
       to  = Date.parse(params[:to])
 
-      @enrollments_counts = Enrollment.where(date: from..to).select(:date).group(:date)
-      @enrollments = Enrollment.where(date: from..to).group_by(&:date)
+      @enrollments = Enrollment.where(date: from..to).order("date").all.group_by(&:date)
       @enrollments_sum = Enrollment.where(date: from..to)
       @enrollments_total = @enrollments_sum.group_by{|e| [e.user_id, e.date]}
       @enrolled_members = Enrollment.where(date: from..to).group_by(&:user_id)
+      @users = User.all
       
-
-      # <th class="dates"><%= (enrollment.date).strftime("%e.%_m.") %>
-      # <% @enrollments.each do |enrollment| %>
-      #     <tr>
-      #      
-      #       <td>  <%= @enrollments_counts %>
-           
-      #     </tr>
-      #   <% end %>
     else
       @enrollments = []
+      @enrollments_total = []
+      @users = []
+      @enrolled_members = []
     end
    
 
