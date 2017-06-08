67675f4d14d1b59f4492fca1642c5928df89c0c5
lkcz04185 <lkcz04185@gmail.com>
2017-06-08 14:31:25 +0000

    stats view

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -6,17 +6,19 @@ class StatsController < ApplicationController
       from = Date.parse(params[:from])
       to  = Date.parse(params[:to])
 
-      @enrollments = Enrollment.where(date: from..to).order("date").all.group_by(&:date)
-      @enrollments_sum = Enrollment.where(date: from..to)
-      @enrollments_total = @enrollments_sum.group_by{|e| [e.user_id, e.date]}
-      @enrolled_members = Enrollment.where(date: from..to).group_by(&:user_id)
+      @enrollments_total = Enrollment.where(date: from..to).order("date").all.group(:date, :user_id).count
+      @enrollments_date = Enrollment.where(date: from..to).order("date").all.group_by(&:date)
+      @enrollments_sel = Enrollment.where(date: from..to).order("date").all.group_by{|e| [e.date, e.user_id]}
+      @enrollments_user = Enrollment.where(date: from..to).order("date").all.group_by(&:user_id)
       @users = User.all
-      
+      @count = 0
+
     else
       @enrollments = []
       @enrollments_total = []
       @users = []
       @enrolled_members = []
+      @enrollments_date = []
     end
    
 
